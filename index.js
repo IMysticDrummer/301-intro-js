1dd29b6ee413f39d2184ee85081ffe72ed70fefd
Iván García Rodríguez <ivan.garcia.rodriguez@hotmail.es>
2022-07-12 23:11:03 +0200

    index.js Mostrar equipos por pantalla

---
+++ index.js
@@ -0,0 +1,18 @@
+/*Mostrar los equipos inscritos por pantalla.
+
+TODO Mostrarla por pantalla.
+TODO Jugar lo partidos de todas las jornadas.
+Una vez terminada cada jornada, se deberá mostrar cómo queda la clasificación de la misma.
+TODO Una vez terminada la liga, se mostrarán estádísticas de número de goles totales y total de puntos ganados.
+*/
+
+// DONE Crear la planificación de jornadas y partidos de cada jornada.
+const TEAMS=[
+  'Osana',
+  'Real Sociedad',
+  'Celta de Vigo',
+  'Cadiz',
+  'Las Palmas',
+  'Hercules'
+]
+console.log(TEAMS)
