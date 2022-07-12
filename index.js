eb91dc3e0beca31b29a47d35aba123bc5447487b
Iván García Rodríguez <ivan.garcia.rodriguez@hotmail.es>
2022-07-12 23:37:41 +0200

    Herencia

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-/*Mostrar los equipos inscritos por pantalla.
-
+/*
 TODO Mostrarla por pantalla.
 TODO Jugar lo partidos de todas las jornadas.
 Una vez terminada cada jornada, se deberá mostrar cómo queda la clasificación de la misma.
 TODO Una vez terminada la liga, se mostrarán estádísticas de número de goles totales y total de puntos ganados.
 */
 
-// DONE Crear la planificación de jornadas y partidos de cada jornada.
+// DONE Mostrar los equipos inscritos por pantalla.
 const TEAMS=[
   'Osana',
   'Real Sociedad',
@@ -16,3 +15,22 @@ const TEAMS=[
   'Hercules'
 ]
 console.log(TEAMS)
+
+//una liga tendrá un conjunto de jornadas
+//cada jornada tendrá un conjunto de partidos
+//un partido es el enfrentamiento de dos equipos
+//el desarrollo de un partido generará un resultado
+
+class League{
+  constructor(name, teams, rounds=1){
+    this.name=name
+    this.teams=teams
+    this.rounds=rounds
+  }
+
+}
+
+const laliga=new League('La Liga', TEAMS)
+console.log(laliga.teams)
+
+// TODO Crear la planificación de jornadas y partidos de cada jornada.
