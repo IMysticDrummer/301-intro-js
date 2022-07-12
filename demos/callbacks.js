fe2efe8891a87f1545d0d74b9ef679c99d96c600
Iván García Rodríguez <ivan.garcia.rodriguez@hotmail.es>
2022-07-12 23:02:25 +0200

    Callbacks modificado con clases

--- demos/callbacks.js
+++ demos/callbacks.js
@@ -1,3 +1,17 @@
+class Personaje{
+  constructor(name, attackSpeed=10, attackDamage=15){
+    this.name=name
+    this.attackSpeed=attackSpeed
+    this.attackDamage=attackDamage
+  }
+
+  //Función de prueba que añade atributos a ese personaje concreto
+  addProp(prop, value){
+    this[prop]= value
+  }
+}
+
+/*
 const legolas={
   name: 'legolas',
   attackSpeed: 20,
@@ -8,6 +22,16 @@ const gimly={
   attackSpeed: 7,
   attackDamage: 15
 }
+*/
+const legolas=new Personaje('legolas',20,10)
+const gimly=new Personaje('gimly', 7, 15)
+const aragorn=new Personaje('aragorn', 7, 15)
+
+//Se pueden crear atributos después de creado el constructor
+aragorn.age=30
+//También con una función dedicada
+aragorn.addProp('charism', 100)
+console.log(aragorn)
 
 function atacar(personaje, attackFunc) {
   let attack=attackFunc(personaje)
@@ -44,3 +68,4 @@ atacar(legolas, attackWithAxe)
 atacar(gimly, attackWithSword)
 atacar(gimly, attackWithArrow)
 atacar(gimly, attackWithAxe)
+atacar(aragorn, attackWithSword)
